b7ff6c2544453cd1584a53055a65ee51eb771071
Kowit C <kowit.s.c@gmail.com>
2014-10-17 06:35:09 +0700

    add user menu

--- app/config/packages/frozennode/administrator/administrator.php
+++ app/config/packages/frozennode/administrator/administrator.php
@@ -1,4 +1,5 @@
 <?php
+
 return array(
 
 	/**
@@ -6,21 +7,20 @@ return array(
 	 *
 	 * @type string
 	 */
-	'uri' => 'admin',
+	'uri' => 'manage',
 
 	/**
 	 * Page title
 	 *
 	 * @type string
 	 */
-	'title' => 'Admin',
+	'title' => 'Hospital Management',
 
 	/**
 	 * The path to your model config directory
 	 *
 	 * @type string
 	 */
-
 	'model_config_path' => app('path') . '/config/administrator',
 
 	/**
@@ -31,27 +31,39 @@ return array(
 	'settings_config_path' => app('path') . '/config/administrator/settings',
 
 	/**
-	 * The menu structure of the site. For models, you should either supply the name of a model config file or an array of names of model config
-	 * files. The same applies to settings config files, except you must prepend 'settings.' to the settings config file name. You can also add
-	 * custom pages by prepending a view path with 'page.'. By providing an array of names, you can group certain models or settings pages
-	 * together. Each name needs to either have a config file in your model config path, settings config path with the same name, or a path to a
-	 * fully-qualified Laravel view. So 'users' would require a 'users.php' file in your model config path, 'settings.site' would require a
-	 * 'site.php' file in your settings config path, and 'page.foo.test' would require a 'test.php' or 'test.blade.php' file in a 'foo' directory
-	 * inside your view directory.
+	 * The menu structure of the site. Each item should either be the name of the model's config file or an array of names of model config files.
+	 * By doing the latter, you can group certain models together. Each name needs to have a config file in your model config path with the same
+	 * name. So 'users' would require a 'users.php' file in your model config path.
 	 *
 	 * @type array
 	 *
 	 * 	array(
-	 *		'E-Commerce' => array('collections', 'products', 'product_images', 'orders'),
-	 *		'homepage_sliders',
+	 *		'Products' => array('products', 'product_images', 'orders'),
 	 *		'users',
-	 *		'roles',
-	 *		'colors',
-	 *		'Settings' => array('settings.site', 'settings.ecommerce', 'settings.social'),
-	 * 		'Analytics' => array('E-Commerce' => 'page.ecommerce.analytics'),
 	 *	)
 	 */
-	'menu' => array(),
+
+	// 'ระบบงานเวชระเบียน'
+	// 'ระบบงานผู้ป่วยนอก'
+	// 'ระบบงานห้องตรวจแพทย์'
+	// 'ระบบงานห้องปฏิบัติการ (LAB)'
+	// 'ระบบงานรังสีวิทยา'
+	// 'ระบบงานเภสัชกรรม'
+	// 'ระบบงานจัดซื้อยาและเวชภัณฑ์'
+	// 'ระบบงานสร้างเสริมสุขภาพ'
+	// 'ระบบงานการเงินผู้ป่วยนอก'
+	// 'ระบบงานผู้ดูแลระบบ (System Admin)'
+	// 'ระบบงาน Online Report'
+	// 'ระบบงานให้รหัสโรค (Coder)'
+	// 'ระบบงานนัดหมายผู้ป่วย'
+	// 'ระบบงานประชาสัมพันธ์'
+	'menu' => array(
+		'Films' => array('films', 'boxoffice'),
+		'users',
+		'directors',
+		'theaters',
+		'Settings' => array('settings.site'),
+	),
 
 	/**
 	 * The permission option is the highest-level authentication check that lets you define a closure that should return true if the current user
@@ -61,7 +73,8 @@ return array(
 	 */
 	'permission'=> function()
 	{
-		return Auth::check();
+		return true;
+		//return Auth::user()->hasRole('admin');
 	},
 
 	/**
@@ -84,40 +97,37 @@ return array(
 	 *
 	 * @type string
 	 */
-	'home_page' => '',
+	'home_page' => 'settings.site',
 
 	/**
-	 * The route to which the user will be taken when they click the "back to site" button
+	 * This is the path where Administrator will send the user if they are not logged in (!Auth::check())
 	 *
 	 * @type string
 	 */
-	'back_to_site_path' => '/',
-
-	/**
-	 * The login path is the path where Administrator will send the user if they fail a permission check
-	 *
-	 * @type string
-	 */
-	'login_path' => 'user/login',
+	'login_path' => 'login',
 
 	/**
 	 * The logout path is the path where Administrator will send the user when they click the logout link
 	 *
 	 * @type string
 	 */
-	'logout_path' => false,
+	'logout_path' => 'logout',
 
 	/**
-	 * This is the key of the return path that is sent with the redirection to your login_action. Session::get('redirect') will hold the return URL.
+	 * Redirect key
 	 *
 	 * @type string
+	 *
+	 * This comes with the redirection to your login_action. Input::get('redirect') will hold the return URL.
 	 */
 	'login_redirect_key' => 'redirect',
 
 	/**
-	 * Global default rows per page
+	 * Global items per page
 	 *
 	 * @type NULL|int
+	 *
+	 * If you set this to an integer value greater than 0, it will override the $per_page static property in ALL of your models
 	 */
 	'global_rows_per_page' => 20,
 
