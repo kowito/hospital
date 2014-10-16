9fa12a36d96c5dd43eb0c0fd8735c09d06fe7bb5
Kowit C <kowit.s.c@gmail.com>
2014-10-16 10:21:04 +0700

    User account template

---
+++ app/views/user/master.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.master')
+@section('nav')
+<div class="navbar navbar-default" role="navigation">
+    <ul class="nav nav-pills nav-stacked">
+        <li><a href="#">ระบบงานเวชระเบียน</a></li>
+        <li><a href="#">ระบบงานเผู้ป่วยนอก</a></li>
+        <li><a href="#">ระบบงานเห้องตรวจแพทย์</a></li>
+        <li><a href="#">ระบบงานเห้องปฏิบัติการ (LAB)</a></li>
+        <li><a href="#">ระบบงานเรังสีวิทยา</a></li>
+        <li><a href="#">ระบบงานเเภสัชกรรม</a></li>
+        <li><a href="#">ระบบงานเจัดซื้อยาและเวชภัณฑ์</a></li>
+        <li><a href="#">ระบบงานเสร้างเสริมสุขภาพ</a></li>
+        <li><a href="#">ระบบงานเการเงินผู้ป่วยนอก</a></li>
+        <li class="active" ><a href="#">ระบบงานเผู้ดูแลระบบ (System Admin)</a></li>
+        <li><a href="#">ระบบงานเ Online Report</a></li>
+        <li><a href="#">ระบบงานเให้รหัสโรค (Coder)</a></li>
+        <li><a href="#">ระบบงานเนัดหมายผู้ป่วย</a></li>
+        <li><a href="#">ระบบงานเประชาสัมพันธ์</a></li>
+    </ul>
+</div>
+@stop
