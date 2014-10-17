873b25d93d0773018a28f439cca38f4dd107315f
Kowit C <kowit.s.c@gmail.com>
2014-10-17 07:02:11 +0700

    All menu

---
+++ app/config/administrator/opd_finance.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Users model config
+ */
+
+return array(
+
+	'title' => 'การเงินผู้ป่วยนอก',
+	'single' => 'การเงินผู้ป่วยนอก',
+	'model' => 'User',
+
+	'columns' => array(
+		'id',
+		'full_name' => array(
+			'title' => 'Name',
+			'select' => "CONCAT((:table).first_name, ' ', (:table).last_name)",
+		),
+		'num_films' => array(
+			'title' => '# email',
+		),
+	),
+
+
+	'filters' => array(
+		'id',
+		'first_name' => array(
+			'title' => 'First Name',
+		),
+		'last_name' => array(
+			'title' => 'Last Name',
+		),
+	),
+
+	'edit_fields' => array(
+		'first_name' => array(
+			'title' => 'First Name',
+			'type' => 'text',
+		),
+		'last_name' => array(
+			'title' => 'Last Name',
+			'type' => 'text',
+		),
+		'email' => array(
+			'title' => 'Email',
+			'type' => 'text',
+		),
+	),
+
+);
