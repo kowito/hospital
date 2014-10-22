d62ea78bb6ccfff2fe9e18f1f1bb8de98100abe6
Kowit C <kowit.s.c@gmail.com>
2014-10-22 12:21:45 +0700

    login bog in a middle

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -4,10 +4,10 @@
         <meta name='viewport' content='width=device-width, initial-scale=1'>
         <title>ระบบงานโรงพยาบาล | @yield('title')</title>
         <link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-        
+
         <?= stylesheet_link_tag() ?>
         <?= javascript_include_tag() ?>
-        
+
     </head>
     <body>
         <div id="main-container">
@@ -16,14 +16,20 @@
                 {{ HTML::image('/assets/logo.png', 'OSGD - Hospital', array('class' => 'logo')) }}
             </div>
             <div class='row'>
+              <div  class="col-md-12">
+                @yield('login')
+              </div>
+            </div>
+
+            <div class='row'>
                 <div  class="col-md-2">
-                    
+
                     @yield('nav')
-                    
+
                 </div>
 
                 <div  class="col-md-10"> @yield('content') </div>
-                
+
             </div>
         </div>
     </body>
